766b1199e9aa2f72866528e14b15e2b0137010da
niedag <105895929+niedag@users.noreply.github.com>
2024-01-29 21:13:21 +0000

    should generate an image of map at specified long/lat

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -17,6 +17,44 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+
+<template>
+  <div id="app">
+    <HereMap  :center="center" />
+  </div>
+</template>
+
+<script>
+import HereMap from './components/HereMap'
+export default {
+  name: 'app',
+  components: {
+    HereMap
+    // Remove the HelloWorld.vue 
+  },
+  data() {
+return {
+    // we are this as prop to the HereMap component 
+  center:{ 
+    lat: 40.730610, 
+    lng: -73.935242
+    }
+}
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
